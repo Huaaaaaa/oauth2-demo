dd00b24eeff97308a20ea75b90967e9e61158f8c
cyhua_csu@163.com <cyhua_csu@163.com>
2021-04-18 12:41:47 +0800

    add routeers

--- src/main/java/com/oauth2/demo/Oauth2DemoApplication.java
+++ src/main/java/com/oauth2/demo/Oauth2DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Oauth2DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oauth2DemoApplication.class, args);
+         SpringApplication.run(Oauth2DemoApplication.class, args);
     }
 
 }
